5343019755c0346a2144bf47d091e52fa13060af
Poojaauma <poojaauma@gmail.com>
2024-11-03 12:02:00 +0530

    Initial Commit

---
+++ src/main/java/com/example/learnspringwchad/LearnspringwchadApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringwchad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringwchadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnspringwchadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learnspringwchad/LearnspringwchadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnspringwchad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringwchadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
