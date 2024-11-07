e34acf612cc95446398053cedc8c9311390aec8c
Poojaauma <poojaauma@gmail.com>
2024-11-07 22:56:10 +0530

    Use of Primary annotation

--- src/main/java/com/example/learnspringwchad/common/coach/TrackCoach.java
+++ src/main/java/com/example/learnspringwchad/common/coach/TrackCoach.java
@@ -1,8 +1,10 @@
 package com.example.learnspringwchad.common.coach;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary // Note Qualifier has highest priority over Primary and Use of Qualifier is recommended
 public class TrackCoach implements Coach{
 
     @Override
